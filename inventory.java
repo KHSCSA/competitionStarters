7a2ce4fade269be6b3a652265a9c0cff5c17c543
heyscottn <heyscottn@gmail.com>
2024-02-27 13:13:01 -0600

    mm

---
+++ inventory.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class inventory {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        // probably a better way to get the data, but we know how many sooo....
+        
+        
+
+    } // close main
+    
+} // close class
