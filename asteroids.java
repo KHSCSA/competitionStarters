9ef3d192f6f7d1d7ed084e3803b00ecc9793af76
heyscottn <heyscottn@gmail.com>
2024-02-27 09:44:09 -0600

    mm

---
+++ asteroids.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class asteroids {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // an ArrayList of Asteroid objects
+        // a 2D list of Strings
+
+
+        // probably a better way to get the data, but we know how many so....
+    
+
+    // close main
+
+
+} // close class
